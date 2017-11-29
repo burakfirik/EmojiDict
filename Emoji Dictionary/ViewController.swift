88e76fe49e181cf7f792e9e913228dd56b7c7a61
Unknown <burakfirik@gmail.com>
2017-11-28 21:46:16 -0500

    create a table view with segue working

---
+++ Emoji Dictionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Burak Firik on 11/27/17.
+//  Copyright © 2017 Code Path. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ EmojiTableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  EmojiTableViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Burak Firik on 11/27/17.
+//  Copyright © 2017 Code Path. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewController: UITableViewController {
+
+    var emojis = ["✊🏻", "👄", "🧠", "🤜🏻", "🙌🏼","👀"]
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+  
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return emojis.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
+  
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    
+    performSegue(withIdentifier: "ourSegue", sender: nil)
+    
+  }
+
+}
